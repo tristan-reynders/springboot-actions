3149ed4baa526e2b09b5b4ccf9b934bd5700425c
tristan-reynders <tristan-reynders@icloud.com>
2024-10-21 22:28:45 +0200

    Fix missing import

--- test-app/src/test/java/com/example/test_app/DemoApplicationTests.java
+++ test-app/src/test/java/com/example/test_app/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.test_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
